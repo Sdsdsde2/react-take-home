ba4bd7c2695e3eb9f1f4e8429ea5985049ae743d
Sdsdsde2 <imspammails31@gmail.com>
2021-08-03 16:40:40 -0500

    added fetchData function

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { render } from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      campaigns: []
+    }
+  }
+
+  fetchData = () => {
+    fetch('https://www.plugco.in/public/take_home_sample_feed')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
