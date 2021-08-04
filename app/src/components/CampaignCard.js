a0aaf3742f6d3b3fbf0bf04ab2eed34333adc393
Sdsdsde2 <imspammails31@gmail.com>
2021-08-03 19:06:32 -0500

    fixed display placements

--- app/src/components/CampaignCard.js
+++ app/src/components/CampaignCard.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import MediaCard from './MediaCard';
 
+import { Carousel } from 'react-responsive-carousel';
+
 import 'antd/dist/antd.css';
-import { Avatar, Carousel } from 'antd';
+import { Avatar } from 'antd';
+
 
 export default class CampaignCard extends Component {
 
@@ -19,10 +22,10 @@ export default class CampaignCard extends Component {
                 <h3 className="campaign-title">{this.props.campaign.campaign_name}</h3>
                 <h3 className="campaign-pay">{this.props.campaign.pay_per_install} per install</h3>
             </div>
-            
-            <div>
+
+            <div className="media-container">
                 {this.props.campaign.medias.map(media => {
-                return <MediaCard media={media} key={media.id} />
+                    return <MediaCard media={media} key={media.id} />
                 })}
             </div>
       </div>
--- app/src/components/MediaCard.js
+++ app/src/components/MediaCard.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 
+import { Carousel } from 'react-responsive-carousel';
+
 export default class MediaCard extends Component {
 
   render() {
     return (
-      <div className="MediaCard">
-            <div className="media-slider">
-                <img src={this.props.media.cover_photo_url} className="campaign-cover"></img>
-                <a href={this.props.media.download_url} download="download">Click to download</a>
-                {/* <button onClick={() => {navigator.clipboard.writeText(this.props.medias[0].tracking_link)}}>Copy Link</button> */}
-                <button onClick={() => {navigator.clipboard.writeText(this.props.media.tracking_link)}} className="campaign-link">Copy Link</button>
-                {/* <a href={this.props.medias[0].download_url} download>Click to download</a> */}
-            </div>
+      <div className="media-slider">
+                    <img src={this.props.media.cover_photo_url} className="campaign-cover"></img>
+                    <a href={this.props.media.download_url} download>
+                        <button onClick={() => {navigator.clipboard.writeText(this.props.medias[0].tracking_link)}} className="campaign-link">Download</button>
+                    </a>
+                    <button onClick={() => {navigator.clipboard.writeText(this.props.media.tracking_link)}} className="campaign-link">Copy</button>
       </div>
     );
   }
